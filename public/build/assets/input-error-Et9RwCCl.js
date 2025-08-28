5af4293eca892d25e79aa6c91fe8336a6d4c2a99
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2025-08-28 20:30:18 +0700

    changes

---
+++ public/build/assets/input-error-Et9RwCCl.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-D8a9I1Yw.js";import{b as o}from"./utils-7eVb4j9J.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
